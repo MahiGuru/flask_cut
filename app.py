543633fe9c207edc2a3a969cd5dcc955f4bc9d71
Mahipal Gurjala <mahipal.gurjala@planonsoftware.com>
2018-11-12 17:12:17 +0530

    Add docker

--- app.py
+++ app.py
@@ -10,31 +10,46 @@ from models import Category, FrontViewType
 app = Flask(__name__)
 # Connect to MongoDB and call the connection "my-app".
 # connect("mongodb://192.168.99.100:27017/flaskCut", alias="flask-cut-db")
-connect("mongodb://localhost:27017/flaskCut", alias="flask-cut-db")
+connect("mongodb://mongodb:27017/flaskCut", alias="flask-cut-db")
 
 
 @app.route('/')
 def todo():
-    _categorys = Category.objects.all()
-    _frontTypes = FrontViewType.objects.all()
-
-    categorys = [item for item in _categorys]
-    frontTypes = [item for item in _frontTypes]
-
-    return render_template('types.html', categorys=categorys, frontTypes=frontTypes)
+    return render_template('index.html')
 
 
-@app.route('/frontviewtype', methods=['POST'])
-def frontviewtype():
-    category_id = ObjectId(request.form['category'])
-    FrontViewType(
-        name=request.form['name'],
-        description=request.form['description'],
-        category=category_id
+@app.route('/category', methods=['POST', 'GET'])
+def category():
+    # Category(
+    #     name=request.form['name'],
+    #     description=request.form['description']
+    # ).save(force_insert=True)
+    Category(
+        name='Electronics',
+        description='All electronic products'
     ).save(force_insert=True)
+    _categorys = Category.objects.all()
+    categorys = [item for item in _categorys]
+    print(_categorys)
     # return redirect(url_for('todo'))
-    return render_template('success.html')
+    return render_template('category.html', categorys=categorys)
+
+@app.route('/frontviewtype', methods=['POST', 'GET'])
+def frontviewtype():
+    # category_id = ObjectId(request.form['category'])
+    # FrontViewType(
+    #     name=request.form['name'],
+    #     description=request.form['description'],
+    #     category=category_id
+    # ).save(force_insert=True)
+    # return redirect(url_for('todo'))
+    _categorys = Category.objects.all()
+    categorys = [item for item in _categorys]
+    _frontTypes = FrontViewType.objects.all()
+    frontTypes = [item for item in _frontTypes]
+    # return redirect(url_for('todo'))
+    return render_template('frontTypes.html', categorys=categorys, frontTypes=frontTypes)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=2500)
+    app.run(debug=True, host='0.0.0.0', port=3000)
