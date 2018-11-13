ef2d72d9ae4c5271ef5fd059fb5756223d94f3ec
Mahipal Gurjala <mahipal.gurjala@planonsoftware.com>
2018-11-13 19:10:46 +0530

    updated

--- app.py
+++ app.py
@@ -5,22 +5,26 @@ from bson.objectid import ObjectId
 from pymongo import MongoClient
 import pprint
 from datetime import datetime
-from utils.common_def import alreadyExists, allowed_file
 from werkzeug.utils import secure_filename
-
+import pathlib
 
 app = Flask(__name__)
 
+from utils.common_def import alreadyExists, allowed_file
 #image info
 app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'uploads')
+app.config['CATEGORY'] = app.config['UPLOAD_FOLDER']+'/'+'categorys'
+
+
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-
+# for DOCKER CONFIG
 # connect("mongodb://192.168.99.100:27017/flaskCut", alias="flask-cut-db")
 # connect("mongodb://mongo:27017/flaskCut", alias="flask-cut-db")
 
 client = MongoClient('mongodb://localhost:27017/')
+# client = MongoClient('mongodb://mongodb:27017/')
 db = client.flaskCut
 # listout all collections
 # pprint.pprint(db.collection_names(include_system_collections=False))
@@ -34,23 +38,10 @@ backViewTypes = db.backViewType
 def todo():
     return render_template('index.html')
 
-def upload_file(filer,name):
-    print(filer)
-    if filer.filename == '':
-        flash('No selected file')
-        return redirect(request.url)
-    if filer and allowed_file(filer.filename):
-        # d = datetime.date.today()
-        # filename = secure_filename('_cat_'+id+'_'+str(datetime.now().strftime('%Y_%m_%d')) +'.'+filer.filename.rsplit('.', 1)[1])
-        filer.save(os.path.join(app.config['UPLOAD_FOLDER'], name))
-
 @app.route('/category', methods=['POST', 'GET'])
 def category():
     if request.method == "POST":
-        category = {
-            'title': request.form['title'],
-            'desc': request.form['desc']
-        }
+        
         insertedId= ''
         if 'images' not in request.files:
             flash('No file part')
@@ -58,16 +49,24 @@ def category():
         checkExists = alreadyExists(categorys, request.form['title'])
         if checkExists:
             print("Ooops you entered with same name")
-        else: 
-            print("still called")
+        else:
+            file111 = [file.filename for file in request.files.getlist("images")]
+            category = {
+                'title': request.form['title'],
+                'desc': request.form['desc']
+            }
             insertedId = categorys.insert_one(category).inserted_id
-            print("\n\n\n\n")
-            print(insertedId)
+            
+        # os.makedirs(os.path.join(app.config['UPLOAD_FOLDER']+'/'+str(insertedId)))    
+        # pathlib.Path(os.path.join(app.config['UPLOAD_FOLDER']+'/'+str(insertedId))).mkdir(parents=True, exist_ok=True)
+        fileNamesArr = []    
         for index, filer in enumerate(request.files.getlist("images")):
-            print(str(insertedId))
             filename = str(index)+'_cat_'+str(insertedId)+'.'+filer.filename.rsplit('.', 1)[1]
-            print(filename)
-            upload_file(filer, filename)
+            fileNamesArr.append(filename)
+            if filer and allowed_file(filer.filename):
+                filer.save(os.path.join(app.config['CATEGORY']), filename)
+        categorys.find_one_and_update({'_id': insertedId}, {'$inc': {'count': 1}, '$set': {'files': fileNamesArr}})
+
         return redirect(url_for('category'))
 
 
--- utils/common_def.py
+++ utils/common_def.py
@@ -1,3 +1,6 @@
+from flask import Flask, request,flash, redirect
+import os
+
 def alreadyExists(collection, title):
     print(collection.find({'title': { "$in": [title]}}).count())
     if collection.find({'title': { "$in": [title]}}).count() > 0:
@@ -9,4 +12,3 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
